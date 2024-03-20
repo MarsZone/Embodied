15088b29284f976a9ccc0815574e50b9a49d54d3
Mars <zm03322161@qq.com>
2024-03-21 00:36:24 +0800

    work on register api

--- server/social/src/main/kotlin/com/mars/social/controller/UserController.kt
+++ server/social/src/main/kotlin/com/mars/social/controller/UserController.kt
@@ -3,15 +3,19 @@ package com.mars.social.controller
 import com.mars.social.model.User
 import com.mars.social.model.Users
 import org.ktorm.database.Database
+import org.ktorm.entity.add
 import org.ktorm.entity.sequenceOf
 import org.ktorm.entity.toList
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.time.LocalDateTime
+import java.time.LocalTime
 
 @RestController
 @RequestMapping("/api/users")
@@ -21,8 +25,14 @@ class UserController {
     protected lateinit var database: Database
     @PostMapping("/register")
     fun registerUser(@RequestBody user: User): ResponseEntity<String> {
-        // 在这里实现用户注册逻辑，验证用户信息，存储用户信息等操作
-        // 返回成功注册提示信息或错误信息
+        val passwordEncoder = BCryptPasswordEncoder()
+        val encryptedPassword = passwordEncoder.encode(user.password)
+        user.password = encryptedPassword
+        user.isActive = "true"
+        user.registerTime = LocalDateTime.now()
+        user.lastLoginTime = LocalDateTime.now()
+        val users = database.sequenceOf(Users)
+        users.add(user);
         return ResponseEntity.ok("用户注册成功")
     }
 
@@ -32,6 +42,6 @@ class UserController {
     @GetMapping("/list")
     fun getAllUsers(): ResponseEntity<List<User>> {
         val users = database.sequenceOf(Users).toList()
-        return ResponseEntity.ok(users)
+        return ResponseEntity.ok().body(users)
     }
 }
