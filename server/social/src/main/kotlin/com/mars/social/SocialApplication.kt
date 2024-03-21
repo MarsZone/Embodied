636e4c33126acfaf714ac2c30d166b3b249c9649
Mars <zm03322161@qq.com>
2024-03-22 02:17:19 +0800

    add login logout isLogin api and with i18n

--- server/social/src/main/kotlin/com/mars/social/SocialApplication.kt
+++ server/social/src/main/kotlin/com/mars/social/SocialApplication.kt
@@ -1,7 +1,7 @@
 package com.mars.social
 
+import cn.dev33.satoken.SaManager
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
 import org.springframework.boot.runApplication
 
 @SpringBootApplication()
@@ -9,4 +9,5 @@ class SocialApplication
 
 fun main(args: Array<String>) {
 	runApplication<SocialApplication>(*args)
+	println("启动成功，Sa-Token 配置如下：" + SaManager.getConfig())
 }
