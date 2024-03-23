83b270957e93ff0d0e13fb81a6f3392799b750c5
Mars <zm03322161@qq.com>
2024-03-24 00:23:56 +0800

    add request dataTime and open cros

--- server/social/src/main/kotlin/com/mars/social/configuration/CustomInterceptor.kt
+++ server/social/src/main/kotlin/com/mars/social/configuration/CustomInterceptor.kt
@@ -3,11 +3,31 @@ package com.mars.social.configuration
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
 import org.springframework.web.servlet.HandlerInterceptor
+import org.springframework.web.servlet.ModelAndView
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 class CustomInterceptor : HandlerInterceptor {
+    override fun preHandle(request: HttpServletRequest, response: HttpServletResponse, handler: Any): Boolean {
+        val startTime = System.currentTimeMillis()
+        request.setAttribute("startTime", startTime)
+        return true
+    }
 
+    override fun postHandle(request: HttpServletRequest, response: HttpServletResponse, handler: Any, modelAndView: ModelAndView?) {
+        val startTime = request.getAttribute("startTime") as Long
+        val endTime = System.currentTimeMillis()
+        val requestTime = endTime - startTime
+
+        val currentTime = LocalDateTime.now()
+        val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
+        val formattedTime = currentTime.format(formatter)
+
+        println("Request URL: ${request.requestURL}, Request Time: $requestTime ms, Time: $formattedTime")
+    }
     override fun afterCompletion(request: HttpServletRequest, response: HttpServletResponse, handler: Any, ex: Exception?) {
         // 设置响应头
         response.setHeader("Access-Control-Expose-Headers", "Authorization")
+        response.setHeader("Access-Control-Allow-Origin", "*")
     }
 }
