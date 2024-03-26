70e67a4ded2ae607789a38af14a314bf9b1aa4ea
mmmml <lucy83844@gmail.com>
2024-03-27 00:20:04 +0800

    add css style for login page

--- client/app/src/pages/Login/Login.jsx
+++ client/app/src/pages/Login/Login.jsx
@@ -10,8 +10,8 @@ const Login = () => {
   const [form] = Form.useForm()
   const dispatch = useDispatch() //在组件中调dispatch方法，需要用钩子函数useDispatch
   const navigate = useNavigate()
-  
-  const onFinish = async (values) => { 
+
+  const onFinish = async (values) => {
     console.log(values)
     //触发异步action fetchLogin
     await dispatch(fetchLogin(values)) //参数就是收集到的表单数据values
@@ -30,41 +30,35 @@ const Login = () => {
         onFinish={onFinish}
         form={form}
         footer={
-          <div style={{ margin: '16px 16px 0' }}>
-            <Button round nativeType='submit' type='primary' block>
-              提交
-            </Button>
+          <div>
+            <Button className='login-button'>登录</Button>
+            <div class="route-to-signup">
+              <p>还没有账户? <a href="#">注册</a></p>
+            </div>
           </div>
         }
       >
-        <h2 className='login-title'>登录</h2>
-        <Form.Item
-          tooltip={{
-            message:
-              'A prime is a natural number greater than 1 that has no positive divisors other than 1 and itself.',
-          }}
-          intro='确保这是唯一的用户名'
+        <p className='login-logo'>Embodied</p>
+        <Form.Item className='login-input'
           rules={[
-            { 
-              required: true, 
-              message: '请填写用户名' 
-            },
             {
-              pattern: /^[a-zA-Z0-9_]{4,16}$/,
-              message: '账号名必须由4-16位的英文字母大小写、数字或下划线组成'
-            }
+              required: true,
+              message: '请填写用户名'
+            },
+            // {
+            //   pattern: /^[a-zA-Z0-9_]{4,16}$/,
+            //   message: '账号名必须由4-16位的英文字母大小写、数字或下划线组成'
+            // }
           ]}
           name='userName' //userName 需要和后端接口保持一致
-          label='用户名'
         >
           <Input placeholder='请输入用户名' />
         </Form.Item>
-        <Form.Item
+        <Form.Item className='login-input'
           rules={[
             { required: true, message: '请填写密码' }
           ]}
           name='password'
-          label='密码'
         >
           <Input placeholder='请输入密码' />
         </Form.Item>
