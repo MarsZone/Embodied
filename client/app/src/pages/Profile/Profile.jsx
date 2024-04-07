84cd9d039ab940d112c7a288306413acc2984bc0
mmmml <lucy83844@gmail.com>
2024-04-08 00:31:43 +0800

    update profile info

--- client/app/src/pages/Profile/Profile.jsx
+++ client/app/src/pages/Profile/Profile.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import TabNavigator from '@/components/TabNavigator/TabNavigator'
-import { Image, NavBar, Toast } from 'react-vant'
+import { Image, NavBar, Toast, Flex, Tabs} from 'react-vant'
+import { Edit } from '@react-vant/icons'
 import './Profile.scss'
 
 const Profile = () => {
+
+  const tabs = ["收藏", "点赞", "历史"];
 
   const src = 'https://img.yzcdn.cn/vant/cat.jpeg'
 
@@ -11,18 +14,50 @@ const Profile = () => {
 
     <div className="layout">
 
+      <NavBar
+        className='nav-bar'
+        title='我的'
+        leftText='返回'
+        onClickLeft={() => Toast('返回')}
+        rightText={<Edit fontSize={20} />}
+        onClickRight={() => Toast('按钮')}
+      />
+
       <div className="container">
         <div className='top-layout'>
           <div className='profile-img'>
             <Image round fit='cover' src={src} />
-            <div className='profile-description'>该用户还没有写简介</div>
           </div>
-          
+          <div className='profile-username'>Embodied</div>
 
+          <div className='profile-social'>
+            <Flex gutter={16}>
+              <Flex.Item span={8}>
+                <div className='count'>111</div>关注
+              </Flex.Item>
+              <Flex.Item span={8}>
+                <div className='count'>222</div>粉丝
+              </Flex.Item>
+              <Flex.Item span={8}>
+                <div className='count'>333</div>获赞
+              </Flex.Item>
+            </Flex>
+          </div>
         </div>
+
         <div className='bottom-layout'>
+          <Tabs className='bottom-tabs'>
+            {tabs.map(
+              item => (
+                <Tabs.TabPane key={item} title={item}>
+                  我是用户信息页Profile
+                </Tabs.TabPane>
+              )
+            )}
+          </Tabs>
           我是用户信息页Profile
         </div>
+
         {/* <Outlet /> */}
       </div>
 
@@ -32,6 +67,8 @@ const Profile = () => {
       <div className="footer">
         <TabNavigator />
       </div>
+
+
     </div>
   )
 }
