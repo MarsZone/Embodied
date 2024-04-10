3b73f9da719680ecf912edef62ecf901eec335e1
WML <lucy83844@gmail.com>
2024-04-10 19:55:42 +0800

    commit

--- client/app/src/pages/Post/Post.jsx
+++ client/app/src/pages/Post/Post.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
-import { Image, NavBar, Toast, Flex } from 'react-vant'
+import { Image, NavBar, Toast, Flex, Button, Input, Form } from 'react-vant'
+import { useNavigate } from 'react-router-dom'
 import TabNavigator from '@/components/TabNavigator/TabNavigator'
 
 const Post = () => {
+
+  const navigate = useNavigate()
+  const [form] = Form.useForm()
+  const onFinish = values => {
+    console.log(values)
+  }
+
+
+
   return (
-
-
 
     <div className="layout">
 
@@ -14,16 +22,39 @@ const Post = () => {
         title='发布话题'
         leftText='返回'
         onClickLeft={() => Toast('返回')}
-        //rightText={<Edit fontSize={20} />}
-        //onClickRight={() => navigate('/userDetail')}
+      //rightText={<Edit fontSize={20} />}
+      //onClickRight={() => navigate('/userDetail')}
       />
-
-
 
       <div className="container">
         我是发布消息页Post
-        {/* <Outlet /> */}
+
+        <Form
+          className='post-form'
+          form={form}
+          onFinish={onFinish}
+          footer={
+            <div style={{ margin: '16px 16px 0' }}>
+              <Button round nativeType='submit' type='primary' block>
+                提交
+              </Button>
+            </div>
+          }
+        >
+
+          <Form.Item
+            rules={[{ required: true, message: '' }]}
+            name='title'
+            label='主题'
+          >
+            <Input placeholder='请输入发布主题' />
+          </Form.Item>
+
+
+        </Form>
       </div>
+
+
       <div className="footer">
         <TabNavigator />
       </div>
