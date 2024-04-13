e45d398a16ab91f75e61dab6da5dfcd31bbc75ac
Mars <zm03322161@qq.com>
2024-04-14 00:40:44 +0800

    get current user message history list

--- server/social/src/main/kotlin/com/mars/social/controller/MessageController.kt
+++ server/social/src/main/kotlin/com/mars/social/controller/MessageController.kt
@@ -6,6 +6,7 @@ import com.mars.social.model.mix.Message
 import com.mars.social.model.mix.Messages
 import com.mars.social.utils.R
 import org.ktorm.database.Database
+import org.ktorm.database.asIterable
 import org.ktorm.entity.Entity
 import org.ktorm.entity.add
 import org.ktorm.entity.sequenceOf
@@ -26,7 +27,7 @@ class MessageController  {
     @SaCheckLogin
     @PostMapping("send")
     fun send(@RequestBody dto:MessageDto):ResponseEntity<R> {
-        val message:Message = Entity.create<Message>()
+        val message:Message = Entity.create<Message >()
         message.msgType="u";
         message.senderUid = StpUtil.getLoginId().toString().toLong()
         message.receiverUid = dto.to
@@ -41,5 +42,37 @@ class MessageController  {
 
         return ResponseEntity.ok().body(R.ok("message send"))
     }
+
+    //Current user, get top x different people message and check status.
+    data class HistoryResult(val senderUid: String, val status: String, val count: Int)
+    @SaCheckLogin
+    @GetMapping("history")
+    fun history(@RequestParam size: Int):ResponseEntity<R>{
+        val uid =StpUtil.getLoginId().toString().toLong()
+        val historyList = database.useConnection { conn ->
+            val sql = """
+                SELECT m.sender_uid, m.status, COUNT(*) AS count from embodied.messages m
+                where status = 'unCheck' and receiver_uid = ?
+                GROUP BY m.sender_uid, m.status
+                ORDER BY MAX(id) DESC
+                limit 0,?
+            """.trimIndent()
+            conn.prepareStatement(sql).use { statement ->
+                statement.setLong(1, uid)
+                statement.setInt(2, size)
+                statement.executeQuery().asIterable().map {
+                    HistoryResult(
+                        it.getString("sender_uid"),
+                        it.getString("status"),
+                        it.getInt("count")
+                    )
+                }
+            }
+        }
+        historyList.forEach{println(it)}
+        return ResponseEntity.ok().body(R.ok(historyList))
+    }
+
+    //Get Unread Top 10 Message List
 
 }
