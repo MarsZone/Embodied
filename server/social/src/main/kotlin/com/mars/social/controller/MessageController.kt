75a4749b99adeb7a4baa3594f8118fe2cd0e6c8f
Mars <zm03322161@qq.com>
2024-05-16 20:55:24 +0800

    rebuild history api

--- server/social/src/main/kotlin/com/mars/social/controller/MessageController.kt
+++ server/social/src/main/kotlin/com/mars/social/controller/MessageController.kt
@@ -7,6 +7,7 @@ import com.mars.social.dto.PageRequest
 import com.mars.social.model.mix.Message
 import com.mars.social.model.mix.Messages
 import com.mars.social.model.topic.Topics
+import com.mars.social.model.user.UserDetails
 import com.mars.social.utils.PageCalculator
 import com.mars.social.utils.R
 import org.ktorm.database.Database
@@ -68,25 +69,22 @@ class MessageController  {
     }
 
     //Current user, get top x different people message and check status.
-    data class HistoryResult(val senderId: String, val senderNickName: String, val senderAvatar:String,
-                             val status: String, val count: Int, val msgType:String, var lastMsg:String)
+    data class HistoryResult(var senderId: String, var senderNickName: String, var senderAvatar:String,
+                             var status: String, var unReadCount: Int, var msgType:String, var lastMsg:String)
     @SaCheckLogin
     @GetMapping("history")
     fun history(@RequestParam size: Int):ResponseEntity<R>{
         val uid =StpUtil.getLoginId().toString().toLong()
         val historyList = database.useConnection { conn ->
             val sql = """
-                SELECT t.sender_id, t.status, t.msg_type, t.count, COALESCE(ud.nick_name, 'unknown') AS senderNickName, 
-                        COALESCE(ud.avatar, '') AS senderAvatar
+                SELECT DISTINCT sender_id
                 FROM (
-                    SELECT m.sender_id, m.status,m.msg_type, COUNT(*) AS count
-                    FROM embodied.messages m
-                    WHERE m.receiver_uid = ?
-                    GROUP BY m.sender_uid, m.status, m.msg_type
-                    ORDER BY Max(m.id) DESC
-                    LIMIT 0, ?
-                ) AS t
-                LEFT JOIN embodied.user_details ud ON ud.uid = t.sender_uid;
+                    SELECT sender_id
+                    FROM messages
+                    WHERE receiver_uid = ? AND sender_id IS NOT NULL
+                    ORDER BY id DESC
+                    LIMIT 0,?
+                ) AS subquery;
             """.trimIndent()
             conn.prepareStatement(sql).use { statement ->
                 statement.setLong(1, uid)
@@ -94,23 +92,49 @@ class MessageController  {
                 statement.executeQuery().asIterable().map {
                     HistoryResult(
                         it.getString("sender_id"),
-                        it.getString("senderNickName"),
-                        it.getString("senderAvatar"),
-                        it.getString("status"),
-                        it.getInt("count"),
-                        it.getString("msg_type"),
+                        "",
+                        "",
+                        "",
+                        0,
+                        "",
                         ""
                     )
                 }
             }
         }
         for(history in historyList){
-            var msg = database.from(Messages).select().where{ Messages.senderId eq history.senderId.toString()}.orderBy(Messages.id.desc())
-                    .limit(1).map { row -> Messages.createEntity(row) }.firstOrNull()
-            if (msg != null) {
-                history.lastMsg = msg.content.toString()
+//            var msg = database.from(Messages).select().where{ Messages.senderId eq history.senderId.toString()}.orderBy(Messages.id.desc())
+//                    .limit(1).map { row -> Messages.createEntity(row) }.firstOrNull()
+//            if (msg != null) {
+//                history.lastMsg = msg.content.toString()
+//            }
+            var lastMsg = database.from(Messages).select().where{(Messages.receiverUid eq uid.toString().toLong()) and (Messages.senderId eq history.senderId)}
+                    .orderBy(Messages.id.desc()).limit(1).map{ row -> Messages.createEntity(row)}.firstOrNull()
+            if (lastMsg != null) {
+                history.lastMsg = lastMsg.content.toString()
+                history.msgType = lastMsg.msgType.toString()
+                history.status = lastMsg.status.toString()
+            }
+            if(history.msgType == "u"){
+                var userDetail = database.from(UserDetails).select().where{ UserDetails.uid eq history.senderId.toString().toLong() }.limit(1).map{row -> UserDetails.createEntity(row)}.firstOrNull()
+                if(userDetail!=null){
+                    history.senderNickName = userDetail.nickName
+                    history.senderAvatar = userDetail.avatar
+                }
+            }else if(history.msgType == "g"){
+                history.senderNickName = "SomeGroup"
+            }else if(history.msgType == "s"){
+                history.senderNickName = "System"
+            }
+            //unread count
+            if(history.status == "unCheck"){
+                var unread = database.from(Messages).select().where{
+                    (Messages.receiverUid eq uid.toString().toLong()) and
+                        (Messages.senderId eq history.senderId) and (Messages.status eq "unCheck") }.totalRecordsInAllPages
+                history.unReadCount = unread
             }
         }
+
         return ResponseEntity.ok().body(R.ok(historyList))
     }
 
