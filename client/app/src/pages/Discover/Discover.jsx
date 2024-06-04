2cde6e0d6d75451f55f2291a8b1f079942ccb5e7
mmmml <lucy83844@gmail.com>
2024-06-05 01:04:14 +0800

    update home page scss style

--- client/app/src/pages/Discover/Discover.jsx
+++ client/app/src/pages/Discover/Discover.jsx
@@ -1,30 +1,18 @@
-import { Outlet, useActionData } from "react-router-dom"
 import TabNavigator from "@/components/TabNavigator/TabNavigator"
-import { Image, NavBar, Toast } from "react-vant"
+import { Image, NavBar, Toast, Tabs } from "react-vant"
 import './Discover.scoped.scss'
-import { previewFileApi } from "@/apis/file"
 
 const Discover = () => {
-
-
-  //根据tid获取url
-  const getUrlByPid = async (pid) => {
-    const res = await previewFileApi(pid)
-    console.log(res)
-    return res.data
-  }
-
-
-  previewFileApi(5).then(data => {
-    const imageUrl = data.data;
-    // 在组件的渲染过程中使用图片 URL 渲染 Image 组件
-    return (
-      <Image round fit='cover' src={imageUrl} />
-    );
-  });
-
-
-  //const url = "http://120.78.142.84:9000/embodied/20240409231020112-5b8bec60-1.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=9mheJLVpHTohzMYlOcXl%2F20240511%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20240511T090600Z&X-Amz-Expires=36000&X-Amz-SignedHeaders=host&X-Amz-Signature=e01a89b2f9d332f3c43199e4d4532d497c03497121453795fb5e84388a2beb80"
+  const tabList = [
+    {
+      key: 'follow',
+      name: '关注'
+    },
+    {
+      key: 'random',
+      name: '随机'
+    }
+  ]
 
   return (
     <div className="layout">
@@ -34,12 +22,21 @@ const Discover = () => {
         onClickRight={() => Toast('按钮')}
       />
       <div className="container">
+
+        <Tabs border type='capsule'>
+          {tabList.map(item => (
+            <Tabs.TabPane
+              key={item.key}
+              title={`${item.name}`}
+              name={item.key}
+            >
+            </Tabs.TabPane>
+          ))}
+        </Tabs>
         {/* 二级路由的出口，如果没有出口就无法显示二级路由 */}
         {/* <Outlet /> 
       我是发现页Discover */}
 
-
-        <Image round fit='cover' src={previewFileApi(5).then(data => data.data)} />
       </div>
       <div className="footer">
         <TabNavigator />
--- client/app/src/pages/Home/Home.jsx
+++ client/app/src/pages/Home/Home.jsx
@@ -1,9 +1,9 @@
-import React, { useEffect} from 'react'
+import React, { useEffect } from 'react'
 import TabNavigator from '@/components/TabNavigator/TabNavigator'
 import {
   Image, NavBar, Toast, Tabs, Sticky, Typography
 } from 'react-vant'
-import { Arrow, CommentO, LikeO, BookmarkO} from '@react-vant/icons'
+import { Arrow, CommentO, LikeO, BookmarkO } from '@react-vant/icons'
 import { useState } from 'react'
 import './Home.scoped.scss'
 import useChannelList from '@/hooks/useChannelList'
@@ -16,16 +16,51 @@ import { getProfileAPI } from '@/apis/user'
 const Home = () => {
   //获取频道列表
   const { channelList, loading } = useChannelList()
-  const [channelTopics, setChannelTopics] = useState([])
+  const [topicList, setTopicList] = useState([])
   const [coverUrl, setCoverUrl] = useState()
+
+  //在组件挂载时，加载初始话题列表
+  const fetchChannelList = async () => {
+    const channelKey = channelList[0].key
+    const res = await getChannelTopicsApi(channelKey)
+    const list = res.data
+
+    //拼接coverUrl
+    const topicListWithCover = await Promise.all(
+      list.map(async topic => {
+        const coverRes = await previewFileApi(topic.coverImg)
+        return { ...topic, coverUrl: coverRes.data }
+      })
+    )
+    console.log('拼接后的topicList：', topicListWithCover)
+    setTopicList(topicListWithCover)
+  }
+
+  //初始化
+  useEffect(() => {
+    if (!loading) {
+      fetchChannelList()
+    }
+  }, [channelList, loading])
+
 
   //点击频道切换
   const onTabClick = async (channel) => {
     const channelKey = await channel.name
     console.log('选中频道：', channelKey)
     //根据切换的channelKey，切换展示的话题
-    const channelTopicsRes = await getChannelTopicsApi(channelKey)
-    setChannelTopics(channelTopicsRes.data)
+    const res = await getChannelTopicsApi(channelKey)
+    const list = res.data
+
+    //拼接coverUrl
+    const topicListWithCover = await Promise.all(
+      list.map(async topic => {
+        const coverRes = await previewFileApi(topic.coverImg)
+        return { ...topic, coverUrl: coverRes.data }
+      })
+    )
+    console.log('拼接后的topicList：', topicListWithCover)
+    setTopicList(topicListWithCover)
   }
 
   //获取发布者用户
@@ -46,102 +81,93 @@ const Home = () => {
   //     });
   // }
 
-  //在组件挂载时，加载初始话题列表
-  const fetchDefaultChannelTopics = async () => {
-    console.log('频道1：', channelList[0].key)
-    const defaultChannelKey = channelList[0].key
-    const channelTopicsRes = await getChannelTopicsApi(defaultChannelKey)
-    setChannelTopics(channelTopicsRes.data)
-  }
-
   //根据channelKey获取name
   const getChannelNameByKey = (key) => {
     const channel = channelList.find(item => item.key === key)
     return channel ? channel.name : ''
   }
 
-  //根据tid获取url
-  const fetchCoverUrl = async () => {
-    const getUrlRes = await previewFileApi(5)
-    setCoverUrl(getUrlRes.data)
-    console.log('获取url：', getUrlRes.data)
-  }
-
-
-  const url = "http://120.78.142.84:9000/embodied/20240409231020112-5b8bec60-1.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=9mheJLVpHTohzMYlOcXl%2F20240511%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20240511T090600Z&X-Amz-Expires=36000&X-Amz-SignedHeaders=host&X-Amz-Signature=e01a89b2f9d332f3c43199e4d4532d497c03497121453795fb5e84388a2beb80"
-  console.log('示例url：', url)
-
-  useEffect(() => {
-    if (!loading) {
-      fetchDefaultChannelTopics()
-
-      fetchCoverUrl()
-    }
-  }, [channelList, loading])
-
-  console.log(channelList)
-
 
   return (
-    <div className="layout">
-
+    <div>
       <Sticky>
         <NavBar
           title="首页"
-          rightText="按钮"
-          onClickRight={() => Toast('按钮')}
+          leftArrow=""
         />
       </Sticky>
 
-      <div className="container">
+      <div className="bg-box">
         <Tabs
-          sticky offsetTop={46}
-          onClickTab={v => onTabClick(v)}>
-          {channelList.map(item => (
-            <Tabs.TabPane key={item.key} title={`${item.name}`} name={item.key}>
+          sticky
+          offsetTop={46}
+          color='#243B55'
+          onClickTab={v => onTabClick(v)}
+        >
 
-              {channelTopics.map((topic, index) => (
-                <div className='topic-outer-box' key={index}>
-                  <div className='topic-inner-box'>
-                    <Link to={`/topicDetail/${topic.id}`} className='topic-link'>
+          {channelList.map(item => (
+            <Tabs.TabPane
+              key={item.key}
+              title={`${item.name}`}
+              name={item.key}
+            >
+              <div className='topic-container'>
+                {topicList.map((topic, index) => (
+                  <div className='topic-box' key={index}>
+                    <a className='topic-box__link' href={`/topicDetail/${topic.id}`} >
                       <div className='topic-header'>
-                        <div className='topic-channel'>{getChannelNameByKey(topic.channelKey)}</div>
-                        <div className='topic-title'>{topic.title}</div>
+                        <div className='topic-header topic-header__title'>
+                          {topic.title}
+                        </div>
+                        <div className='topic-header topic-header__channel'>
+                          {getChannelNameByKey(topic.channelKey)}
+                        </div>
                         <Arrow></Arrow>
                       </div>
-                    </Link>
+                    </a>
 
-                    <Typography.Text
-                      className='topic-content'
-                      ellipsis={2}>
+                    {/* <Typography.Text
+                    className='topic-content'
+                    ellipsis={2}>
+                    内容：{topic.content}
+                  </Typography.Text> */}
+
+                    <div className='topic-content'>
                       内容：{topic.content}
-                    </Typography.Text>
+                    </div>
 
                     <div className='topic-cover'>
-                      <Image
-                        src={coverUrl}
-                        fit='cover' />
+                      {topic.coverImg ? (
+                        <Image
+                          src={topic.coverUrl}
+                          fit='cover'
+                        />
+                      ) : (
+                        <div></div>
+                      )}
                     </div>
 
                     <div className='topic-bottom'>
                       <div>
-                        发布者{topic.authorUid}·{topic.updateTime}
+                        uid{topic.authorUid} · {topic.updateTime}
                       </div>
-                      <div className='topic-bottom-right'>
+                      <div className='topic-bottom topic-bottom__right'>
                         <div> <LikeO />{topic.likes}</div>
                         <div> <BookmarkO />{topic.bookmarks}</div>
                         <div> <CommentO />{topic.comments}</div>
                       </div>
                     </div>
                   </div>
-                </div>
-              ))
-              }
+                ))
+                }
+              </div>
             </Tabs.TabPane>
           ))}
+
         </Tabs>
 
       </div>
+
       <div className="footer">
         <TabNavigator />
       </div>
